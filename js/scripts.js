fd840e6cefa8b51dc432207cc75052c6d3f7724d
Brian <ballerbrian4@gmail.com>
2017-12-04 19:05:10 -0800

    added css and js folders and files

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+$(document).ready(
+    function () {
+
+
+    }
+    );
+
+
+function submitColorOrder() {
+    
+
+}
+
+$("#red-button").on('click', function () {
+    var text = $('#input-string');
+
+    text.val(text.val + ' 1')
+
+})
+
+$("#orange-button").on('click', function () {
+    var text = $('#input-string');
+
+    text.val(text.val + ' 2')
+
+
+})
+    
+$("#yellow-button").on('click', function () {
+    var text = $('#input-string');
+
+    text.val(text.val + ' 3')
+
+
+})
+
+$("#green-button").on('click', function () {
+    var text = $('#input-string');
+
+    text.val(text.val + ' 4')
+
+
+})
+
+$("#blue-button").on('click', function () {
+    var text = $('#input-string');
+
+    text.val(text.val + ' 5')
+
+
+})
+
+$("#white-button").on('click', function () {
+    var text = $('#input-string');
+
+    text.val(text.val + ' 6')
+
+
+})
